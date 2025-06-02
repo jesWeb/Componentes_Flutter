d4b22f8da9540bd769bfa71ab07dd197f1165abf
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2025-06-02 13:02:44 -0600

    agregfamos mas imagenes mediante argumentos a su vez creamos variables para que sea dinamico y agregamos una condicion para mostrar si trae nombre

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/widgets.dart';
+
 class CardScreen extends StatelessWidget {
   const CardScreen({super.key});
 
@@ -10,17 +11,34 @@ class CardScreen extends StatelessWidget {
         title: const Text('Card Widget'),
       ),
       body: ListView(
-        //estsos son los atributos que recibe 
+        //estsos son los atributos que recibe
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         children: [
           CustomCard1(),
-          SizedBox(height: 10,),
-          CustomCardType2()
+          SizedBox(
+            height: 10,
+          ),
+          CustomCardType2(name: 'porche 1'
+            ,
+              imageUrl:
+                  'https://images-porsche.imgix.net/-/media/9CD53A02737F40A0806BB5EAE1A80F54_D92CC07A446F4B0A8A492A88DD260C0C_911_carrera_gts_sideshot?w=600&q=45&crop=faces%2Centropy%2Cedges&auto=format'),
+          SizedBox(
+            height: 10,
+          ),
+          CustomCardType2(
+              imageUrl:
+                  'https://images-porsche.imgix.net/-/media/3E21D30B38B9493DB0857BBBEF18A927_D65EC42E368B435398BE6AD275F02F59_CM24N3KIX0002-718-cayman-gts-40-side?w=1400&q=45&crop=faces%2Centropy%2Cedges&auto=format'),
+          SizedBox(
+            height: 10,
+          ),
+          CustomCardType2(
+              imageUrl:
+                  'https://images-porsche.imgix.net/-/media/E27875C9513D4899B596093AC53B7407_8EA111E8A92F4A7BAC344DAD79DA99EE_718-Boxster-GTS-40-sideshot?w=600&q=45&crop=faces%2Centropy%2Cedges&auto=format'),
+          SizedBox(
+            height: 10,
+          ),
         ],
       ),
     );
   }
-
 }
-
-
--- lib/widgets/Custom_card_type_2.dart
+++ lib/widgets/Custom_card_type_2.dart
@@ -1,31 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  //imagen dinamica
+  final String imageUrl;
+  final String? name;
+
+  const CustomCardType2({super.key, required this.imageUrl, this.name});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 25,
       child: Column(
         children: [
-          const FadeInImage(
-            image: NetworkImage(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9S8NLgMf2GzqmXb80_7cSeEy5JUnaozltYA&s'),
-            placeholder: AssetImage('assets/jar-loading.gif'),
+          FadeInImage(
+            image: NetworkImage(imageUrl),
+            // image: NetworkImage(
+            //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9S8NLgMf2GzqmXb80_7cSeEy5JUnaozltYA&s'),
+            placeholder: const AssetImage('assets/jar-loading.gif'),
             width: double.infinity,
             height: 230,
-            fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fit: BoxFit.contain,
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
-          Container(
-              alignment: AlignmentDirectional.centerEnd,
-              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-              child: Text('Una Hermosa sala Minimalista'))
+          if (name != null)
+            Container(
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                child: Text(name ?? 'No titulo'))
         ],
       ),
     );
